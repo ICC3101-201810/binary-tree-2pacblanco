e2b33fbc2d6c70d54770bff3c9bdd403240bb72d
ING <jcfiguero@miuandes.cl>
2018-04-24 16:08:44 -0300

    LAB NODOS

---
+++ arbol/arbol/ArbolBinario.cs
@@ -0,0 +1,33 @@
+﻿using Nodo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arbol
+{
+    class ArbolBinario<T>
+    {
+        Nodo <T> cabeza;
+
+        public ArbolBinario()
+        {
+            cabeza = null;
+        }
+        
+        public void Insertar(List<int> lista, int info)
+        {
+            foreach(int i in lista)
+            {
+                if(i==0)
+                {
+
+                }
+            }
+        }
+
+   
+    }
+  
+}
